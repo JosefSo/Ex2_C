75ee5b2d4832f5f478a0fce2d436b3df2a09d8f3
JosefSo <77780368+JosefSo@users.noreply.github.com>
2021-11-23 12:00:44 +0200

    Add files via upload

---
+++ my_mat.h
@@ -0,0 +1,9 @@
+#ifndef MY_MAT_H
+#define MY_MAT_H
+
+void scanMatrix(int matrix[][COLS], int rows, int cols);
+bool isExistsPath(int i, int j);
+int shortestPath(int i, int j);
+
+
+#endif //MY_MAT_H
